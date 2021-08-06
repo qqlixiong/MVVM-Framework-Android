9aff1631597aa6b08ae3531e93aae833f6082e61
lixiong <qqlixiongjob@163.com>
2021-08-06 13:20:56 +0800

    ......

--- mvvm-framework/src/main/java/com/lx/framework/utils/StringUtils.java
+++ mvvm-framework/src/main/java/com/lx/framework/utils/StringUtils.java
@@ -1,9 +1,15 @@
 package com.lx.framework.utils;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
+
+import java.util.List;
 
 import androidx.annotation.ColorInt;
 
@@ -253,6 +259,28 @@ public final class StringUtils {
             chars[len - i - 1] = c;
         }
         return new String(chars);
+    }
+
+    /**
+     * 判断某个activity是否在前台显示
+     */
+    public static boolean isForeground(Activity activity) {
+        return isForeground(activity, activity.getClass().getName());
+    }
+
+    /**
+     * 判断某个界面是否在前台,返回true，为显示,否则不是
+     */
+    private static boolean isForeground(Activity context, String className) {
+        if (context == null || TextUtils.isEmpty(className))
+            return false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> list = am.getRunningTasks(1);
+        if (list != null && list.size() > 0) {
+            ComponentName cpn = list.get(0).topActivity;
+            return className.equals(cpn.getClassName());
+        }
+        return false;
     }
 
     /**
